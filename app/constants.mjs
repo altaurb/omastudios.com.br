0654a7788d38a1b304004e192e3aed6c5b9938a8
Claudio G. Ferraz <contact@claudioferraz.com>
2026-01-28 14:07:58 -0300

    initial commit

--- app/constants.mjs
+++ app/constants.mjs
@@ -8,6 +8,19 @@ export const assetBaseUrl = "/assets/";
 /**
  * @type {import("@webstudio-is/image").ImageLoader}
  */
-export const imageLoader = ({ src }) => {
-  return src;
+export const imageLoader = (props) => {
+  if (process.env.NODE_ENV !== "production") {
+    return props.src;
+  }
+
+  if (props.format === "raw") {
+    return props.src;
+  }
+
+  // https://vercel.com/blog/build-your-own-web-framework#automatic-image-optimization
+  const searchParams = new URLSearchParams();
+  searchParams.set("url", props.src);
+  searchParams.set("w", props.width.toString());
+  searchParams.set("q", props.quality.toString());
+  return `/_vercel/image?${searchParams}`;
 };
